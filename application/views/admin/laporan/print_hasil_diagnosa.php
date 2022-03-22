434db0035416411002bfd4c7299e62aa133d6f0b
agitaerfan <agita.erfan001@gmail.com>
2022-03-22 23:59:08 +0700

    improve

--- application/views/admin/laporan/print_hasil_diagnosa.php
+++ application/views/admin/laporan/print_hasil_diagnosa.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="<?php echo base_url('assets/frontend/') ?>/surat/style1.css" />
+    <link rel="stylesheet" href="<?php echo base_url('assets/client/') ?>/surat/style1.css" />
     <title>Laporan Hasil Laporan Diagnosa </title>
   </head>
    <body>
